483b565a48dcf23b786c519de575da6cc1671cad
Jordan Read <jread6477@gmail.com>
2021-10-07 22:32:27 -0400

    Customer Edit view and functionality added

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -37,5 +37,61 @@ namespace GeneralStore.MVC.Controllers
 
             return View(customer);
         }
+
+        //GET: Delete
+        //Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        //POST: Delete
+        //Customer/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Edit
+        //Customer/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        //POST: Edit
+        //Customer/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
     }
 }
